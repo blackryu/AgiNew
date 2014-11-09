7b829d4b3f0cb693b519f87d168ebecfca603424
quiN68 <philip.quintus@gmail.com>
2014-11-09 20:01:28 +0100

    hoffentlich fertig

--- agi2/src/agi2/FourWinsLogicImpl.java
+++ agi2/src/agi2/FourWinsLogicImpl.java
@@ -1,6 +1,6 @@
 package agi2;
 
-public class FourWinsLogicImpl {
+public class FourWinsLogicImpl implements FourWinsLogic {
 
 	Chip playingField[][] = new Chip[6][7];
 	Result status;
@@ -21,32 +21,110 @@ public class FourWinsLogicImpl {
 	}
 
 	private Result checkResult(Chip color, int column) {
-		if (isPlayingFieldFull())
-			return Result.tied;
-
 		for (int i = playingField.length - 1; i >= 0; i--) {
 			if (playingField[i][column] == color) {
+				// 3 nach oben/unten testen
 				if (i >= playingField.length - 3) {
 					if (playingField[i - 1][column] == color
 							&& playingField[i - 2][column] == color
 							&& playingField[i - 3][column] == color) {
 						return Result.won;
-					} else if (column < 4) {
-						if (playingField[i][column + 1] == color
-								&& playingField[i][column + 2] == color
-								&& playingField[i][column + 3] == color) {
-							return Result.won;
-						} else if (column > 2) {
-							if (playingField[i][column - 1] == color
-									&& playingField[i][column - 2] == color
-									&& playingField[i][column - 3] == color) {
-								return Result.won;
-							}
-						}
+					}
+				}
+				// 3 nach rechts testen
+				if (column < 4) {
+					if (playingField[i][column + 1] == color
+							&& playingField[i][column + 2] == color
+							&& playingField[i][column + 3] == color) {
+						return Result.won;
+					}
+				} // 3 nach links testen
+				if (column > 2) {
+					if (playingField[i][column - 1] == color
+							&& playingField[i][column - 2] == color
+							&& playingField[i][column - 3] == color) {
+						return Result.won;
+					}
+				} // 1 nach links 2 nach rechts
+				if (column > 0 && column < 5) {
+					if (playingField[i][column - 1] == color
+							&& playingField[i][column + 1] == color
+							&& playingField[i][column + 2] == color) {
+						return Result.won;
+					}
+				} // 2 nach links 1 nach rechts
+				if (column > 1 && column < 6) {
+					if (playingField[i][column - 1] == color
+							&& playingField[i][column + 1] == color
+							&& playingField[i][column - 2] == color) {
+						return Result.won;
+					}
+				}// 3 diagonal nach rechts oben
+				if (column < 4 && i >= playingField.length - 3) {
+					if (playingField[i - 1][column + 1] == color
+							&& playingField[i - 2][column + 2] == color
+							&& playingField[i - 3][column + 3] == color) {
+						return Result.won;
+					}
+				}// 2 diagonal rechts oben 1 links runter
+				if (column < 5 && column > 0 && i <= playingField.length - 2
+						&& i >= playingField.length - 4) {
+					if (playingField[i - 1][column + 1] == color
+							&& playingField[i - 2][column + 2] == color
+							&& playingField[i + 1][column - 1] == color) {
+						return Result.won;
+					}
+				}// 1 rechts oben 2 links unten
+				if (column < 6 && column > 1 && i <= playingField.length - 3
+						&& i >= playingField.length - 5) {
+					if (playingField[i - 1][column + 1] == color
+							&& playingField[i + 2][column - 2] == color
+							&& playingField[i + 1][column - 1] == color) {
+						return Result.won;
+					}
+				}// 3 links unten
+				if (column > 2 && i <= playingField.length - 4) {
+					if (playingField[i + 1][column - 1] == color
+							&& playingField[i + 2][column - 2] == color
+							&& playingField[i + 3][column - 3] == color) {
+						return Result.won;
+					}
+				}// 3 links hoch
+				if (column > 2 && i >= playingField.length - 4) {
+					if (playingField[i - 1][column - 1] == color
+							&& playingField[i - 2][column - 2] == color
+							&& playingField[i - 3][column - 3] == color) {
+						return Result.won;
+					}
+				}
+				// 3 rechts unten
+				if (column < 4 && i <= playingField.length - 4) {
+					if (playingField[i + 1][column + 1] == color
+							&& playingField[i + 2][column + 2] == color
+							&& playingField[i + 3][column + 3] == color) {
+						return Result.won;
+					}
+				}// 2 rechts unten 1 links oben
+				if (column < 5 && column > 0 && i <= playingField.length - 2
+						&& i >= playingField.length - 5) {
+					if (playingField[i + 1][column + 1] == color
+							&& playingField[i + 2][column + 2] == color
+							&& playingField[i - 1][column - 1] == color) {
+						return Result.won;
+					}
+				}// 1 rechts unten 2 links oben
+				if (column < 6 && column > 1 && i <= playingField.length - 2
+						&& i >= playingField.length - 4) {
+					if (playingField[i + 1][column + 1] == color
+							&& playingField[i - 1][column - 1] == color
+							&& playingField[i - 2][column - 2] == color) {
+						return Result.won;
 					}
 				}
 			}
 		}
+		if (isPlayingFieldFull())
+			return Result.tied;
 
 		return Result.continues;
 	}
@@ -61,7 +139,6 @@ public class FourWinsLogicImpl {
 				i = -1;
 			}
 		}
-
 	}
 
 	public boolean isPlayingFieldFull() {
